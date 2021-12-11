8680fe12772ff22f6005cefcbaaa827f6325e523
wolfpirker <w_pirker@gmx.de>
2021-12-11 21:12:18 +0100

    about pure components

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
+import Header from './components/header'
+import Home from './components/home';
+import PureComp from './components/pureComp';
+import PureCompF from './components/pureCompF';
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Header />
+      <div className="container">
+        <Switch>
+          <Route path="/purecomp" component={PureComp} />
+          <Route path="/purecompf" component={PureCompF} />
+          <Route path="/" component={Home} />
+        </Switch>
+      </div>
+    </BrowserRouter>
   );
 }
 
---
+++ src/components/pureComp.js
@@ -0,0 +1,36 @@
+import React, { PureComponent } from 'react';
+
+
+class PureComp extends PureComponent {
+    state = {
+        name: 'Francis'
+    }
+
+    // in a normal class based component we could do...
+    // shouldComponentUpdate(nextProps,nextState){
+    //     if(nextState.name === this.state.name){
+    //         return false
+    //     }
+    //     return true;
+    // }
+
+
+    render() {
+        console.log('render')
+        return (
+            <>
+                <h3>{this.state.name}</h3>
+                <button
+                    onClick={
+                        () => this.setState({ name: 'Francis' })
+                    }
+                >
+                    Click to change
+                </button>
+            </>
+        )
+    }
+}
+
+
+export default PureComp;
